b1bbc0f143b69efac82d9da0dc49bd9dfcb7499e
Luis Pérez <ljpm9401@gmail.com>
2023-11-14 07:06:21 -0500

    Ajuste video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,9 +12,7 @@
     
     figure.mt-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-
-
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/KM-RhabB6qs" title="Visión general del comercio electrónico" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     
 </template>
